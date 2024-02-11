92629d1ba919e3cec7ed3930fb817dd18cc869dd
HermanErKu <herman.lindalen.nodeland@outlook.com>
2024-02-12 00:03:24 +0100

    added page title

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 
 function App() {
@@ -19,6 +19,11 @@ function App() {
         if (newIndex > 9) { newIndex = 0 };
         setNoButtonIndex(newIndex);
     }
+
+    useEffect(() => {
+        document.title = 'Vil du bli min Valentine?';
+    }, []);
+    
 
 
     if (pressedYes) {
